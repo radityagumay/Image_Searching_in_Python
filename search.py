d3a5c19f7bc797cafa5b273992c61c62d153da84
radityagumay <gumay.raditya@yahoo.com>
2016-10-11 18:52:27 +0700

    first commit

--- search.py
+++ search.py
@@ -22,18 +22,25 @@ for (query, queryFeatures) in index.items():
     cv2.imshow("Query", newimage)
     print "query: %s" % (query)
 
-    montageA = np.zeros((170 * 50, 170, 3), dtype="uint8")
-    for j in xrange(0, 50):
+    listImage1 = np.zeros((170 * 5, 170, 3), dtype="uint8")
+    listImage2 = np.zeros((170 * 5, 170, 3), dtype="uint8")
+    for j in xrange(0, 10):
         (score, imageName) = results[j]
         path = directory + "/%s" % (imageName)
         image = cv2.imread(path)
         newx, newy = image.shape[1] / 6, image.shape[0] / 6
         newimage = cv2.resize(image, (newx, newy))
-        cv2.imshow(imageName, newimage)
+        #cv2.imshow(imageName, newimage)
         print "\t%d. %s : %.3f" % (j + 1, imageName, score)
-    cv2.waitKey(0)
+        # check to see if the first montage should be used
+        if j < 5:
+            listImage1[j * 170:(j + 1) * 170, :] = newimage
+
+        # otherwise, the second montage should be used
+        else:
+            listImage2[(j - 5) * 170:((j - 5) + 1) * 170, :] = newimage
 
     # show the results
-    #cv2.imshow("Show results", montageA)
-    #cv2.waitKey(0)
-    #print "size", len(montageA)
+    cv2.imshow("Results 1-5", listImage1)
+    cv2.imshow("Results 6-10", listImage2)
+    cv2.waitKey(0)
